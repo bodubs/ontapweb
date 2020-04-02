4aae6c1a0e222572fa6a4622af2ae70d08f8350e
bodubs <30421760+bodubs@users.noreply.github.com>
2020-04-02 09:49:28 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,12 +1,18 @@
-
 <!-- %%%%%%%%%%%%%%%%%%%%%%     Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
 <header>
-    <img src="ontap-logo.png" alt="On Tap Logo"/>  
+    <img src="ontap-logo.png" alt="On Tap Logo" id="ontap-logo"/>  
     <?php
     include 'nav.php';
     ?>
     
 </header>
 <!-- %%%%%%%%%%%%%%%%%%%%% Ends Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
+<?php
+	if ($PATH_PARTS['filename'] == 'register') {
+            print '<div id="center-logo">
+    				<img src="OnTapAdmin-logo.png" alt="ontapadmin logo" id="ontapadmin-logo">
+					</div>';
+    }
+?>
 <div id="container">
 	<div id="main">
