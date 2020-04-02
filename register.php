5ca1449df11a740f7da32fc11094cf3418b82154
bodubs <30421760+bodubs@users.noreply.github.com>
2020-04-02 09:50:17 -0400

    Update register.php

--- register.php
+++ register.php
@@ -59,18 +59,20 @@ if($file){
 fclose($file);
 
 ?>
-
+<!-- <div id="center-logo">
+    <img src="OnTapAdmin-logo.png" alt="ontapadmin logo" id="ontapadmin-logo">
+</div> -->
 <h2>Want to Get Your Bar on OnTap?</h2>
+<h3>Step 1: Fill out the following form</h3>
 
-<h3>To Register Your Bar for OnTap, please fill out the following form:</h3>
+<img src="download-on-the-app-store-apple.svg" alt="apple-app-store-download" id="download-app" class="app-store-download">
 
 <form id="register-form">
-	<input type="text" placeholder="First Name" name="firstName"><br>
-	<input type="text" placeholder="Last Name" name="lastName"><br>
-	<input type="text" placeholder="Email" name="email"><br>
-	<input type="text" placeholder="Phone" name="phone"><br>
-	<input type="text" placeholder="Bar Name" name="barName"><br>
-	<input type="text" placeholder="Bar Street Address" name="barAddress">
+	<input type="text" placeholder="First Name" name="firstName" class="long-input"><br>
+	<input type="text" placeholder="Last Name" name="lastName" class="long-input"><br>
+	<input type="text" placeholder="Email" name="email" class="long-input"><br>
+	<p id="full-phone-num"><input type="text" placeholder="XXX" name="phone-area-code" class="phone" maxlength="3">-<input type="text" placeholder="XXX" name="phone-exchange-code" class="phone" maxlength="3">-<input type="text" placeholder="XXXX" name="phone-line-number" id="line-num" maxlength="4"></p><br><input type="text" placeholder="Bar Name" name="barName" class="long-input"><br>
+	<input type="text" placeholder="Bar Street Address" name="barAddress" class="long-input">
 	<select name="state">
 		<option value=""></option>
 		<?php
@@ -88,10 +90,20 @@ fclose($file);
 		?>
 		<!-- NEED TO FILL IN W STATES -->
 	</select>
-	<input type="text" placeholder="Zip Code" name="zipCode"><br>
+	<input type="text" placeholder="Zip Code" name="zipCode" class="long-input"><br>
     <input type="text" placeholder="Optional..." name="comment" id="comment"><br>
 	<input type="button" value="Submit" id="btnSubmit">
 </form>
+
+<h3>Step 2: Download OnTapAdmin</h3>
+<img src="ontap-admin-app-thumbnail.png" alt="Ontap Admin app thumbnail" id="admin-app-thumbnail"><br>
+<img src="download-on-the-app-store-apple.svg" alt="apple-app-store-download" class="app-store-download">
+
+<h3>Step 3: Please wait to be contacted by us for verification and log-in information!</h3>
+
+<div id="features-section-admin">
+    <h3>OnTap Admin</h3>
+</div>
 
 <?php
 
