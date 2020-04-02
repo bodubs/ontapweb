6e1af4cbb0182cbc485efd2115a4907b8dda5649
bodubs <30421760+bodubs@users.noreply.github.com>
2020-04-02 09:47:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,14 @@ include 'top.php';
 
 ?>
 
-<h1>Download our mobile app</h1>
+<div id="home-page-app-download">
+	<h4>Download Our Mobile App!</h4>
+	<img src="ontap-app-icon.png" alt="OnTap App Icon" id="ontap-app-icon"><br>
+	<img src="download-on-the-app-store-apple.svg" alt="apple-app-store-download" class="app-store-download">
+</div>
+
+<h1>Know Where to Go!</h1>
+
 
 <?php
 
